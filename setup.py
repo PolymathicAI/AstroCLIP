37c9af7f243ab37dac2d489d9f48d0cbcaf68ada
Francois Lanusse <fr.eiffel@gmail.com>
2023-09-30 13:41:08 -0400

    popupaling setup

--- setup.py
+++ setup.py
@@ -6,16 +6,11 @@ setup(
     packages=find_packages(),
     install_requires=[
         'torch',
-        'matplotlib',
-        'scipy',
+        'lightning',
+        'timm',
+        'datasets'
     ],
-    entry_points={
-        'console_scripts': [
-            'astroclip=astroclip.cli:main',
-        ],
-    },
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A short description of your project',
+    author='EiffL',
+    description='Cross-Modal Pretraining for Astronomical Images',
     url='https://github.com/yourusername/astroclip',
 )
