25a633601d98ce8fedcef23c171e5b9d4ca8fd09
Francois Lanusse <EiffL@users.noreply.github.com>
2023-10-02 09:02:08 -0400

    Update train_redshift.py

--- scripts/train_redshift.py
+++ scripts/train_redshift.py
@@ -16,7 +16,7 @@ def main():
                                name="spectrum2redshift")
 
     # Load dataset
-    dataset = load_dataset('EiffL/AstroCLIP')
+    dataset = load_dataset('../astroclip/datasets/legacy_survey.py')
     dataset.set_format(type='torch', columns=['spectrum', 'redshift'])
 
     train_loader = DataLoader(dataset['train'], batch_size=64, 
