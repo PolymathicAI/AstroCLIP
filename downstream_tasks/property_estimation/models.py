f991070321ce8d531fee322087c34d712c882e3f
lhparker1 <liamholdenparker@gmail.com>
2024-04-23 11:55:08 -0400

    downstream tasks

---
+++ downstream_tasks/property_estimation/models.py
@@ -0,0 +1,211 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+    
+class ResNet18(nn.Module):
+    """
+    ResNet18 neural network for galaxy image processing.
+    
+    """
+    def __init__(self, num_classes=1):
+        
+        super(ResNet18, self).__init__()
+        self.resnet = models.resnet18(weights=None)
+        self.resnet.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        return self.resnet(x)    
+    
+class SimpleMLP(nn.Module):
+    """
+    Simple MLP for CLIP feature few-shot regression
+    """
+    def __init__(self, input_dim=128, output_dim=1, hidden_dim=64):
+        super(SimpleMLP, self).__init__()
+        self.fc1 = nn.Linear(input_dim,hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim,int(hidden_dim/2))
+        self.fc3 = nn.Linear(int(hidden_dim/2), output_dim)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x.squeeze()
+    
+class MLP(nn.Sequential):
+    """Multi-Layer Perceptron
+
+    A simple implementation with a configurable number of hidden layers and
+    activation functions.
+
+    Parameters
+    ----------
+    n_in: int
+        Input dimension
+    n_out: int
+        Output dimension
+    n_hidden: list of int
+        Dimensions for every hidden layer
+    act: list of callables
+        Activation functions after every layer. Needs to have len(n_hidden) + 1
+        If `None`, will be set to `LeakyReLU` for every layer.
+    dropout: float
+        Dropout probability
+    """
+
+    def __init__(self, n_in, n_out, n_hidden=(16, 16, 16), act=None, dropout=0):
+
+        if act is None:
+            act = [
+                nn.LeakyReLU(),
+            ] * (len(n_hidden) + 1)
+        assert len(act) == len(n_hidden) + 1
+
+        layer = []
+        n_ = [n_in, *n_hidden, n_out]
+        for i in range(len(n_) - 1):
+            layer.append(nn.Linear(n_[i], n_[i + 1]))
+            layer.append(act[i])
+            layer.append(nn.Dropout(p=dropout))
+
+        super(MLP, self).__init__(*layer)
+
+
+class SpectrumEncoder(nn.Module):
+    """Spectrum encoder
+
+    Modified version of the encoder by Serrà et al. (2018), which combines a 3 layer CNN
+    with a dot-product attention module. This encoder adds a MLP to further compress the
+    attended values into a low-dimensional latent space.
+
+    Paper: Serrà et al., https://arxiv.org/abs/1805.03908
+
+    Parameters
+    ----------
+    instrument: :class:`spender.Instrument`
+        Instrument that observed the data
+    n_latent: int
+        Dimension of latent space
+    n_hidden: list of int
+        Dimensions for every hidden layer of the :class:`MLP`
+    act: list of callables
+        Activation functions after every layer. Needs to have len(n_hidden) + 1
+        If `None`, will be set to `LeakyReLU` for every layer.
+    dropout: float
+        Dropout probability
+    """
+
+    def __init__(
+        self, n_latent, n_hidden=(32, 32), act=None, dropout=0
+    ):
+
+        super(SpectrumEncoder, self).__init__()
+        self.n_latent = n_latent
+        
+        filters = [8, 16, 16, 32]
+        sizes = [5, 10, 20, 40]
+        self.conv1, self.conv2, self.conv3, self.conv4 = self._conv_blocks(
+            filters, sizes, dropout=dropout
+        )
+        self.n_feature = filters[-1] // 2
+
+        # pools and softmax work for spectra and weights
+        self.pool1, self.pool2, self.pool3 = tuple(
+            nn.MaxPool1d(s, padding=s // 2) for s in sizes[:3]
+        )
+        self.softmax = nn.Softmax(dim=-1)
+
+        # small MLP to go from CNN features to latents
+        if act is None:
+            act = [nn.PReLU(n) for n in n_hidden]
+            # last activation identity to have latents centered around 0
+            act.append(nn.Identity())
+        self.mlp = MLP(self.n_feature, self.n_latent, n_hidden=n_hidden, act=act, dropout=dropout)
+
+    def _conv_blocks(self, filters, sizes, dropout=0):
+        convs = []
+        for i in range(len(filters)):
+            f_in = 1 if i == 0 else filters[i - 1]
+            f = filters[i]
+            s = sizes[i]
+            p = s // 2
+            conv = nn.Conv1d(
+                in_channels=f_in,
+                out_channels=f,
+                kernel_size=s,
+                padding=p,
+            )
+            norm = nn.InstanceNorm1d(f)
+            act = nn.PReLU(f)
+            drop = nn.Dropout(p=dropout)
+            convs.append(nn.Sequential(conv, norm, act, drop))
+        return tuple(convs)
+
+    def _downsample(self, x):
+        # compression
+        x = x.unsqueeze(1)
+        x = self.pool1(self.conv1(x))
+        x = self.pool2(self.conv2(x))
+        x = self.pool3(self.conv3(x))
+        x = self.conv4(x)
+        C = x.shape[1] // 2
+        # split half channels into attention value and key
+        h, a = torch.split(x, [C, C], dim=1)
+
+        return h, a
+
+    def forward(self, y):
+        """Forward method
+
+        Parameters
+        ----------
+        y: `torch.tensor`, shape (N, L)
+            Batch of observed spectra
+
+        Returns
+        -------
+        s: `torch.tensor`, shape (N, n_latent)
+            Batch of latents that encode `spectra`
+        """
+        # run through CNNs
+        h, a = self._downsample(y)
+        # softmax attention
+        a = self.softmax(a)
+
+        # attach hook to extract backward gradient of a scalar prediction
+        # for Grad-FAM (Feature Activation Map)
+        if ~self.training and a.requires_grad == True:
+            a.register_hook(self._attention_hook)
+
+        # apply attention
+        x = torch.sum(h * a, dim=2)
+
+        # run attended features into MLP for final latents
+        x = self.mlp(x)
+        return x
+
+    @property
+    def n_parameters(self):
+        """Number of parameters in this model"""
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+    def _attention_hook(self, grad):
+        self._attention_grad = grad
+
+    @property
+    def attention_grad(self):
+        """Gradient of the attention weights
+
+        Factor to compute the importance of attention for Grad-FAM method.
+
+        Requires a previous `loss.backward` call for any scalar loss function based on
+        outputs of this class's `forward` method. This functionality is switched off
+        during training.
+        """
+        if hasattr(self, "_attention_grad"):
+            return self._attention_grad
+        else:
+            return None
