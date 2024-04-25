bd589cf001bfaf801fd4ec99ee0c6e40df52653e
Leopoldo <lsarra@flatironinstitute.org>
2024-04-25 14:55:06 -0400

    implement original preprocessing

--- astroclip/specformer/__init__.py
+++ astroclip/specformer/__init__.py
@@ -1,4 +1,4 @@
 from . import utils
 from .data import AstroClipDataset
 from .model import SpecFormer
-from .preprocessing import spectrum_collate_fn
+from .preprocessing import SpectrumCollator
--- astroclip/specformer/preprocessing.py
+++ astroclip/specformer/preprocessing.py
@@ -1,56 +1,85 @@
 import numpy as np
 import torch
+from torch.nn.utils.rnn import pad_sequence
 from torch.utils.data import default_collate
 
 
-def slice(x, section_length=10, overlap=5):
-    start_indices = np.arange(0, len(x) - overlap, section_length - overlap)
-    sections = [x[start : start + section_length] for start in start_indices]
+class SpectrumCollator:
+    def __init__(self, num_chunks, chunk_width):
+        self.num_chunks = num_chunks
+        self.chunk_width = chunk_width
 
-    # If the last section is not of length 'section_length', you can decide whether to keep or discard it
-    if len(sections[-1]) < section_length:
-        sections.pop(-1)  # Discard the last section
+    def __call__(self, samples, mlm=None):
+        preprocessed_samples = [
+            self.preprocess(el["spectrum"])
+            for el in samples
+            if np.array(el["spectrum"]).std() > 0
+        ]
+        out = {
+            "input": pad_sequence(
+                [self.mask_seq(torch.tensor(el)) for el in preprocessed_samples],
+                batch_first=True,
+                padding_value=0,
+            ),
+            "target": pad_sequence(
+                [torch.tensor(el) for el in preprocessed_samples],
+                batch_first=True,
+                padding_value=0,
+            ),
+        }
 
-    return np.concatenate(sections, 1).T
+        return out
 
-
-def preprocess(samples):
-    out = []
-
-    for x in samples["spectrum"]:
+    def preprocess(self, x):
         x = np.array(x)
         std, mean = x.std(), x.mean()
         # skipping samples that are all zero
-        if std == 0:
-            continue
-        x = (x - mean) / std
-        x = slice(x, 194, 97)
-        x = np.pad(x, pad_width=((1, 0), (2, 0)), mode="constant", constant_values=0)
+        if std != 0:
+            x = (x - mean) / std
+            x = self.slice(x, 20, 10)
+            x = np.pad(
+                x, pad_width=((1, 0), (2, 0)), mode="constant", constant_values=0
+            )
 
-        x[0, 0] = (mean - 2) / 2
-        x[0, 1] = (std - 2) / 8
+            x[0, 0] = (mean - 2) / 2
+            x[0, 1] = (std - 2) / 8
 
-        out.append(x)
-    # print(len(out))
-    return torch.tensor(np.array(out))
+            return x
+        else:
+            return None
 
+    def mask_seq(self, seq):
+        # randomly masking contiguous sections of the sequence
+        # making sure the separation between chunks is at least chunk_width
 
-# for training we drop chunks of the spectrum
-def drop_chunks(batch, size=5):
-    batch = batch.clone()
-    # random start location between 0 and length of the spectrum
-    start1 = torch.randint(0, batch.shape[1] - 3 * size - 2, (1,)).item()
-    start2 = torch.randint(
-        start1 + size + 1, batch.shape[1] - 2 * size - 1, (1,)
-    ).item()
-    start3 = torch.randint(start2 + size + 1, batch.shape[1] - size, (1,)).item()
-    batch[:, start1 : start1 + size] *= 0
-    batch[:, start2 : start2 + size] *= 0
-    batch[:, start3 : start3 + size] *= 0
-    return batch
+        len_ = seq.shape[0]
+        num_chunks = self.num_chunks
+        chunk_width = self.chunk_width
 
+        # Ensure there's enough space for the chunks and separations
+        total_width_needed = (
+            self.num_chunks * self.chunk_width
+            + (self.num_chunks - 1) * self.chunk_width
+        )
+        if total_width_needed > len_:
+            raise ValueError("Sequence is too short to mask")
 
-def spectrum_collate_fn(sample):
-    sample = default_collate(sample)
-    preprocessed_sample = preprocess(sample)
-    return {"input": drop_chunks(preprocessed_sample), "target": preprocessed_sample}
+        masked_seq = seq.clone()
+
+        for i in range(num_chunks):
+            start = (i * len_) // num_chunks
+            loc = torch.randint(0, len_ // num_chunks - chunk_width, (1,)).item()
+            masked_seq[loc + start : loc + start + chunk_width] = 0
+
+        return masked_seq
+
+    @staticmethod
+    def slice(x, section_length=10, overlap=5):
+        start_indices = np.arange(0, len(x) - overlap, section_length - overlap)
+        sections = [x[start : start + section_length] for start in start_indices]
+
+        # If the last section is not of length 'section_length', you can decide whether to keep or discard it
+        if len(sections[-1]) < section_length:
+            sections.pop(-1)  # Discard the last section
+
+        return np.concatenate(sections, 1).T
