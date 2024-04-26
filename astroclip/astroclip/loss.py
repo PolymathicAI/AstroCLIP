b31dcf92f97f6513a3962acc666954595e210cdf
lhparker1 <liamholdenparker@gmail.com>
2024-04-26 15:15:53 -0400

    pre-commit loss

--- astroclip/astroclip/loss.py
+++ astroclip/astroclip/loss.py
@@ -1,27 +1,41 @@
-import torch
-from torch import nn
-import torch.nn.functional as F
-
-from jaxtyping import Array
 from typing import Tuple
+
+import torch
+import torch.nn.functional as F
+from jaxtyping import Array
+from torch import nn
+
 
 class CLIPLoss(nn.Module):
     def get_logits(
-            self, image_features: Array[torch.FloatTensor, "b c"], spectrum_features: Array[torch.FloatTensor, "b c"], logit_scale: float
-        ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        self,
+        image_features: Array[torch.FloatTensor, "b c"],
+        spectrum_features: Array[torch.FloatTensor, "b c"],
+        logit_scale: float,
+    ) -> Tuple[torch.FloatTensor, torch.FloatTensor]:
+        # Normalize image features
         image_features = F.normalize(image_features, dim=-1, eps=1e-3)
+
+        # Normalize spectrum features
         spectrum_features = F.normalize(spectrum_features, dim=-1, eps=1e-3)
+
+        # Calculate the logits for the image and spectrum features
         logits_per_image = logit_scale * image_features @ spectrum_features.T
         return logits_per_image, logits_per_image.T
 
     def forward(
-        self, image_features: Array[torch.FloatTensor, "b c"], spectrum_features: Array[torch.FloatTensor, "b c"], logit_scale: float, output_dict: bool = False
+        self,
+        image_features: Array[torch.FloatTensor, "b c"],
+        spectrum_features: Array[torch.FloatTensor, "b c"],
+        logit_scale: float,
+        output_dict: bool = False,
     ) -> torch.FloatTensor:
-        """Calculate the contrastive loss for the given image and spectrum features."""
-        
+        # Get the logits for the image and spectrum features
         logits_per_image, logits_per_spectrum = self.get_logits(
             image_features, spectrum_features, logit_scale
         )
+
+        # Calculate the contrastive loss
         labels = torch.arange(
             logits_per_image.shape[0], device=image_features.device, dtype=torch.long
         )
