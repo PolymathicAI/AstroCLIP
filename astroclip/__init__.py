9b8aef5cbe4fb17e481856375fdc8aa1044345ef
lhparker1 <liamholdenparker@gmail.com>
2024-05-01 16:09:51 -0400

    bug fix

--- astroclip/__init__.py
+++ astroclip/__init__.py
@@ -1,6 +1,4 @@
-from . import models, modules
+from . import datasets, models, modules
 from .callbacks import CustomSaveConfigCallback, CustomWandbLogger, PlotsCallback
-from .datasets.datamodule import AstroClipDataloader
-from .datasets.preprocessing import AstroClipCollator
 from .env import format_with_env
 from .scheduler import CosineAnnealingWithWarmupLR
